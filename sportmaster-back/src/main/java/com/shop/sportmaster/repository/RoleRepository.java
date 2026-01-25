7d0419f76667c423f4b3e3dda4f879842ff84af4
dovlatyanc <dovlatyanc.ru@gmail.com>
2026-01-25 13:47:36 +0300

    added repositories

--- sportmaster-back/src/main/java/com/shop/sportmaster/repository/RoleRepository.java
+++ sportmaster-back/src/main/java/com/shop/sportmaster/repository/RoleRepository.java
@@ -1,4 +1,10 @@
 package com.shop.sportmaster.repository;
 
-public class RoleRepository {
+import com.shop.sportmaster.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
